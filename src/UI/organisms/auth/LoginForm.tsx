376bc655d88e3860290627e9603337280438f761
daniel1775 <danilog1000@gmail.com>
2026-04-11 15:43:58 -0500

    feat: call login api in login form

--- src/UI/organisms/auth/LoginForm.tsx
+++ src/UI/organisms/auth/LoginForm.tsx
@@ -1,11 +1,15 @@
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Pressable, Text } from 'react-native';
+import { useState } from 'react';
+
+import { useQueryLoginUser } from '@/src/hooks/queries/useQueryLoginUser';
 
 import { FormTextField } from '@/src/UI/atoms/form/FormTextField';
-import { useState } from 'react';
 
 export const LoginForm = () => {
 	const [email, setEmail] = useState('');
 	const [password, setPassword] = useState('');
+
+	const { submitLogin } = useQueryLoginUser();
 
 	return (
 		<View style={styles.container}>
@@ -21,6 +25,19 @@ export const LoginForm = () => {
 				value={password}
 				onChangeText={setPassword}
 			/>
+			<Pressable
+				onPress={() => submitLogin({ email, password })}
+				style={{
+					width: '100%',
+					height: 50,
+					backgroundColor: '#dfca32ff',
+					borderRadius: 5,
+					justifyContent: 'center',
+					alignItems: 'center',
+				}}
+			>
+				<Text>Login</Text>
+			</Pressable>
 		</View>
 	);
 };
--- src/hooks/queries/useQueryLoginUser.tsx
+++ src/hooks/queries/useQueryLoginUser.tsx
@@ -4,10 +4,17 @@ import { TypeLoginUser } from '@/src/types/auth';
 import { fetchPostLogin } from '@/src/api/fetchPostLogin';
 
 export const useQueryLoginUser = () => {
-	return useMutation({
+	const mutation = useMutation({
 		mutationKey: ['login'],
 		mutationFn: async (payload: TypeLoginUser) => {
 			return fetchPostLogin(payload);
 		},
 	});
+
+	return {
+		submitLogin: mutation.mutateAsync,
+		isLoading: mutation.isPending,
+		isError: mutation.isError,
+		error: mutation.error,
+	};
 };
