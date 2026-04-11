72287852676e4adc651996cabc1511f4272bdd50
daniel1775 <danilog1000@gmail.com>
2026-04-11 13:22:31 -0500

    feat: routes file added

---
+++ src/utils/routes.ts
@@ -0,0 +1,11 @@
+export const POST_LOGIN_ENDPOINT =
+	'https://qf5k9fspl0.execute-api.us-east-1.amazonaws.com/default/login';
+
+export const GET_BALANCE_ENDPOINT =
+	'https://2k0ic4z7s5.execute-api.us-east-1.amazonaws.com/default/balance';
+
+export const POST_TRANSFER_ENDPOINT =
+	'https://ofqx4zxgcf.execute-api.us-east-1.amazonaws.com/default/transfer';
+
+export const GET_TRANSFER_LIST_ENDPOINT =
+	'https://n0qaa2fx3c.execute-api.us-east-1.amazonaws.com/default/transferList';
