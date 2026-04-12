820a1b188693199c83a7ecb577720163c10575bd
daniel1775 <danilog1000@gmail.com>
2026-04-12 06:36:26 -0500

    feat: show correct names on tab items

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -3,9 +3,24 @@ import { Tabs } from 'expo-router';
 export default function TabLayout() {
 	return (
 		<Tabs screenOptions={{ headerShown: false }}>
-			<Tabs.Screen name='index' />
-			<Tabs.Screen name='transfer' />
-			<Tabs.Screen name='transfer-list' />
+			<Tabs.Screen
+				name='index'
+				options={{
+					title: 'Balance',
+				}}
+			/>
+			<Tabs.Screen
+				name='transfer'
+				options={{
+					title: 'Transfer',
+				}}
+			/>
+			<Tabs.Screen
+				name='transfer-list'
+				options={{
+					title: 'List',
+				}}
+			/>
 		</Tabs>
 	);
 }
