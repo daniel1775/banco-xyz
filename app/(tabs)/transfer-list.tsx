e6928606201acd9ac906d84c30f13fcbd32992ff
daniel1775 <danilog1000@gmail.com>
2026-04-12 20:24:06 -0500

    feat: implement tag filter logic

--- app/(tabs)/transfer-list.tsx
+++ app/(tabs)/transfer-list.tsx
@@ -1,5 +1,5 @@
 import { FlatList } from 'react-native';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import { useQueryGetTransferList } from '@/src/hooks/queries/useQueryGetTransferList';
 import { ScreenLayout } from '@/src/UI/layouts/ScreenLayout';
@@ -9,8 +9,36 @@ import { TransferFilter } from '@/UI/molecules/transfer/TranferFilter';
 
 export default function TransferListScreen() {
 	const { transferList } = useQueryGetTransferList();
+
 	const [search, setSearch] = useState('');
 	const [filter, setFilter] = useState<'payeer' | 'value'>('payeer');
+	const [transferListToShow, setTransferListToShow] = useState(transferList);
+
+	useEffect(() => {
+		const handleFilter = () => {
+			if (!search) {
+				setTransferListToShow(transferList);
+			}
+
+			if (filter === 'payeer') {
+				const transferFiltered = transferList?.filter((item) =>
+					item.payeer.name.toLowerCase().includes(search.toLowerCase()),
+				);
+				setTransferListToShow(transferFiltered);
+				return;
+			}
+
+			if (filter === 'value') {
+				const transferFiltered = transferList?.filter((item) =>
+					item.value.toString().includes(search),
+				);
+				setTransferListToShow(transferFiltered);
+				return;
+			}
+		};
+
+		handleFilter();
+	}, [transferList, search]);
 
 	return (
 		<ScreenLayout>
@@ -22,7 +50,7 @@ export default function TransferListScreen() {
 				setFilter={setFilter}
 			/>
 			<FlatList
-				data={transferList}
+				data={transferListToShow}
 				renderItem={({ item }) => <TransferCard transferData={item} />}
 				keyExtractor={(item, index) => index.toString()}
 				style={{
